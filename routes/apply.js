d5b3a202c07061431c3dfdff3ac0110c254a2f15
tsalm0n <tsalm0n@twc.com>
2019-01-24 14:54:10 -0500

    added application form page that puts user data into database

--- routes/apply.js
+++ routes/apply.js
@@ -2,8 +2,119 @@ var express = require('express');
 var router = express.Router();
 
 /* GET apply page. */
-router.get('/', function(req, res, next) {
-  res.render('apply', { title: 'Application Form' });
+router.get('/', function (req, res, next) {
+  res.render('apply', {
+    title: 'Application Form'
+  });
+});
+
+//app.get('/', (request, response) => response.sendFile(`${path.join(__dirname, '../index-form-post-ddb.html')}`)); //(__dirname, '../) goes one folder up from current location 
+
+router.post('/formPost', (req, res, next) => { //take POST request data from teamster apply page & put into database table
+  const postBody = req.body;
+  console.log(postBody);
+
+  //the following puts 'postBody' json into dynamodb database/////////////////////////////////////////
+  //can/should this be made more modular? <--maybe not...
+  var AWS = require('aws-sdk');
+  var dyn = new AWS.DynamoDB({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+    accessKeyId: 'DEFAULT_ACCESS_KEY', // needed if you don't have aws credentials at all in env
+    secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
+  });
+
+  var params = {
+    TableName: "teamster-application-database",
+    Item: { // a map of attribute name to AttributeValue
+      union_number: {
+        'S': Object.values(postBody)[0][0]
+      },
+      lname: {
+        'S': Object.values(postBody)[0][1]
+      },
+      fname: {
+        'S': Object.values(postBody)[0][2]
+      },
+      mi: {
+        'S': Object.values(postBody)[0][3]
+      },
+      occupation: {
+        'S': Object.values(postBody)[0][4]
+      },
+      address: {
+        'S': Object.values(postBody)[0][5]
+      },
+      phone: {
+        'S': Object.values(postBody)[0][6]
+      },
+      city: {
+        'S': Object.values(postBody)[0][7]
+      },
+      state: {
+        'S': Object.values(postBody)[0][8]
+      },
+      zip: {
+        'S': Object.values(postBody)[0][9]
+      },
+      employer: {
+        'S': Object.values(postBody)[0][10]
+      },
+      employment_date: {
+        'S': Object.values(postBody)[0][11]
+      },
+      employer_address: {
+        'S': Object.values(postBody)[0][12]
+      },
+      employer_phone: {
+        'S': Object.values(postBody)[0][13]
+      },
+      employer_city: {
+        'S': Object.values(postBody)[0][14]
+      },
+      employer_state: {
+        'S': Object.values(postBody)[0][15]
+      },
+      employer_zip: {
+        'S': Object.values(postBody)[0][16]
+      },
+      fee: {
+        'S': Object.values(postBody)[0][17]
+      },
+      paid_to: {
+        'S': Object.values(postBody)[0][18]
+      },
+      dob: {
+        'S': Object.values(postBody)[0][19]
+      },
+
+      membership: {
+        'S': Object.values(postBody)[0][21]
+      },
+      previous_union_number: {
+        'S': Object.values(postBody)[0][22]
+      },
+
+      ssn: { //'ssn' is primary key for 'teamster-application-database' table; must always be included in 'putItem' method
+        'S': Object.values(postBody)[0][20]
+      },
+    }
+  };
+
+  dyn.putItem(params, function (err, data) {
+    if (err) console.log(err); // an error occurred
+    else console.log(data); // successful response
+  });
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+  for (n = 0; n < 23; n++) {
+    console.log(Object.values(postBody)[0][n]);
+  }
+
+  console.log(postBody);
+  console.log(Object.values(postBody)[0]);
+
+
 });
 
 module.exports = router;
