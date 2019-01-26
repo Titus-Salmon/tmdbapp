c0b407b570062bf8ef6ba5ce33ca072ed19fccb6
tsalm0n <tsalm0n@twc.com>
2019-01-25 23:51:51 -0500

    very slight mods to app.js & added css to style.css

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs'); //t0d
 
 app.use(logger('dev'));
 app.use(express.json());
